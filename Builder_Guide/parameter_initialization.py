477a08c76b1907ae18ff88f28c7047cd6ba62d3c
wangzhenguang <2215566216@qq.com>
2024-06-18 15:01:43 +0800

    commit CNN

--- Builder_Guide/parameter_initialization.py
+++ Builder_Guide/parameter_initialization.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+from d2l import torch as d2l
 
 def block1():
     return nn.Sequential(nn.Linear(4, 8), nn.ReLU(),
@@ -22,10 +23,12 @@ def my_init(m):
         nn.init.uniform_(m.weight, -10, 10)
         m.weight.data *= m.weight.data.abs() >= 5
 
+
 if __name__ == '__main__':
-    net = nn.Sequential(nn.Linear(4, 8), nn.ReLU(), nn.Linear(8, 1))
-    X = torch.rand(size=(2, 4))
-    Y = net(X)
+    # net = nn.Sequential(nn.Linear(4, 8), nn.ReLU(), nn.Linear(8, 1))
+    # X = torch.rand(size=(2, 4))
+    # Y = net(X)
+
     # print(Y)
     # print(net[2].state_dict())
     # print(type(net[2].bias))
@@ -51,15 +54,23 @@ if __name__ == '__main__':
     # net[0].weight.data[0, 0] = 42
     # print(net[0].weight.data[0])
 
-    # 我们需要给共享层一个名称，以便可以引用它的参数
-    shared = nn.Linear(8, 8)
-    net = nn.Sequential(nn.Linear(4, 8), nn.ReLU(),
-                        shared, nn.ReLU(),
-                        shared, nn.ReLU(),
-                        nn.Linear(8, 1))
+    # # 我们需要给共享层一个名称，以便可以引用它的参数
+    # shared = nn.Linear(8, 8)
+    # net = nn.Sequential(nn.Linear(4, 8), nn.ReLU(),
+    #                     shared, nn.ReLU(),
+    #                     shared, nn.ReLU(),
+    #                     nn.Linear(8, 1))
+    # net(X)
+    # # 检查参数是否相同
+    # print(net[2].weight.data[0] == net[4].weight.data[0])
+    # net[2].weight.data[0, 0] = 100
+    # # 确保它们实际上是同一个对象，而不只是有相同的值
+    # print(net[2].weight.data[0] == net[4].weight.data[0])
+
+    net = nn.Sequential(nn.LazyLinear(256), nn.ReLU(), nn.LazyLinear(10))
+    print(net[0].weight)
+
+    X = torch.rand(2, 20)
     net(X)
-    # 检查参数是否相同
-    print(net[2].weight.data[0] == net[4].weight.data[0])
-    net[2].weight.data[0, 0] = 100
-    # 确保它们实际上是同一个对象，而不只是有相同的值
-    print(net[2].weight.data[0] == net[4].weight.data[0])
+
+    print(net[0].weight.shape)
